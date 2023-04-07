7141b65f006813bc4e7f1951cc19a489d5e76778
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2023-04-07 16:24:04 -0300

    feat: create connection class with rabbitmq

--- src/main/kotlin/com/microservice/estoquepreco/connections/RabbitMQConnection.kt
+++ src/main/kotlin/com/microservice/estoquepreco/connections/RabbitMQConnection.kt
@@ -1,10 +1,34 @@
 package com.microservice.estoquepreco.connections
 
-import org.springframework.stereotype.Component
+import com.microservice.estoquepreco.enums.RabbitMQEnums
+import org.springframework.amqp.core.AmqpAdmin
+import org.springframework.amqp.core.Binding
 import org.springframework.amqp.core.Queue
+import org.springframework.amqp.core.DirectExchange
+import org.springframework.stereotype.Component
 
+//Classe de configuração de conexão com RabbitMQ
 @Component
-class RabbitMQConnection {
+class RabbitMQConnection(private val amqpAdmin: AmqpAdmin) {
 
-    fun queue(name: String) = Queue(name, true, true, true)
+    companion object {
+        const val EXCHANGE_NAME: String = "amq.direct"
+    }
+
+     init {
+        val queueStock = Queue(RabbitMQEnums.ESTOQUE_QUEUE.value, true, true, true)
+        val queuePrice = Queue(RabbitMQEnums.ESTOQUE_PRECO.value, true, true, true)
+        val directExchange = DirectExchange(EXCHANGE_NAME)
+
+        val stockBinding = Binding(queueStock.name, Binding.DestinationType.QUEUE, directExchange.name, queueStock.name, null)
+        val stockPrice = Binding(queuePrice.name, Binding.DestinationType.QUEUE, directExchange.name, queuePrice.name, null)
+
+        amqpAdmin.declareQueue(queueStock)
+        amqpAdmin.declareQueue(queuePrice)
+
+        amqpAdmin.declareExchange(directExchange)
+
+        amqpAdmin.declareBinding(stockBinding)
+        amqpAdmin.declareBinding(stockPrice)
+    }
 }
---
+++ src/main/kotlin/com/microservice/estoquepreco/enums/RabbitMQEnums.kt
@@ -0,0 +1,6 @@
+package com.microservice.estoquepreco.enums
+
+enum class RabbitMQEnums(val value: String) {
+    ESTOQUE_QUEUE("ESTOQUE"),
+    ESTOQUE_PRECO("PRECO")
+}
