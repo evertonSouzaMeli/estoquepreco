46e73d19d2c5b202f630245c7633c9cf191a7e09
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2023-04-06 23:10:15 -0300

    first commit

---
+++ src/main/kotlin/com/microservice/estoquepreco/EstoqueprecoApplication.kt
@@ -0,0 +1,11 @@
+package com.microservice.estoquepreco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstoqueprecoApplication
+
+fun main(args: Array<String>) {
+    runApplication<EstoqueprecoApplication>(*args)
+}
